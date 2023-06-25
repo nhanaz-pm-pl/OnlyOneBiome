d2e7a736916f1a4db20107773f7eac2096eb4102
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-06-25 10:33:05 +0700

    ILoveJungle

---
+++ src/Main.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NhanAZ\ILoveJungle;
+
+use pocketmine\data\bedrock\BiomeIds;
+use pocketmine\event\Listener;
+use pocketmine\event\world\ChunkLoadEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\world\format\SubChunk;
+use pocketmine\world\format\PalettedBlockArray;
+
+class Main extends PluginBase implements Listener {
+
+	protected function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	/** Thanks @Muqsit [https://discord.com/channels/373199722573201408/373214753147060235/1122366583050866738] */
+	public function onChunk(ChunkLoadEvent $event): void {
+		$chunk = $event->getChunk();
+		foreach ($chunk->getSubChunks() as $y => $subChunk) {
+			$chunk->setSubChunk($y, new SubChunk(
+				$subChunk->getEmptyBlockId(),
+				$subChunk->getBlockLayers(),
+				new PalettedBlockArray(BiomeIds::JUNGLE),
+				$subChunk->getBlockSkyLightArray(),
+				$subChunk->getBlockLightArray()
+			));
+		}
+	}
+}
